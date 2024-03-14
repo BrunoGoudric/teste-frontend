bf62bbfb82f8e4962e175a29b2075af67623f43e
Bruno Ferreira <brunolfsports@hotmail.com>
2024-03-14 12:07:14 -0300

    feat:create structure of TableHeader

--- src/components/TableHeader/index.tsx
+++ src/components/TableHeader/index.tsx
@@ -3,7 +3,14 @@ import { Container } from "./styles";
 export default function TableHeader(){
     return (
         <Container>
-            <h1>TableHeader</h1>
+            <div className="boxCompany">
+                <button className="filterButton">Empresa 1</button>
+                <button className="filterButton">Empresa 2</button>
+            </div>
+            <div className="boxSearch">
+                <input type="search" name="search" placeholder="Informe o que precisa...." />
+                <button>Buscar</button>
+            </div>
         </Container>
     )
 }
--- src/components/TableHeader/styles.ts
+++ src/components/TableHeader/styles.ts
@@ -1,3 +1,69 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 150px;
+    width: 100%;
+    margin-top: 50px;
+
+    .boxCompany{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        gap: 25px;
+
+        .filterButton{
+            width: 163px;
+            height: 48px;
+            border-radius: 12px;
+            border: none;
+            transition: 0.2ms;
+        }
+
+        .filterButton:hover{
+            background: #429BA1;
+            color: #fff;
+            font-weight: bold;
+        }
+
+        
+    }
+
+    .boxSearch{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        gap: 25px;
+
+        > input{
+            width: 433px;
+            height: 53px;
+            border-radius: 12px;
+            border: none;
+            padding: 0 20px;
+        }
+
+        > button {
+            width: 140px;
+            height: 53px;
+            border-radius: 12px;
+            border: none;
+
+            color: #fff;
+            font-size: 18px;
+            background: #023D68;
+            font-weight: bold;
+            transition: 0.2ms;
+
+        }
+
+        > button:hover{
+            background: #09568E;
+        }
+    }
+`;
